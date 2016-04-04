94c86a937369e33fdde131d3078b29c04e209996
Rodney Folz <rodney@rodneyfolz.com>
2016-04-04 16:20:45 -0700

    Fix syntax errors in metainfo_test

--- test/bento/metainfo_test.exs
+++ test/bento/metainfo_test.exs
@@ -2,15 +2,15 @@ defmodule Bento.MetainfoTest do
   use ExUnit.Case, async: true
 
   import Bento.Metainfo
-  alias Bento.DecodeError
+  alias Bento.MetainfoError
 
   test "invalid torrent file raises an error" do
-    assert_raises DecodeError, fn -> Bento.torrent!("d3:foo3:bare")
+    assert_raise MetainfoError, fn -> Bento.torrent!("d3:foo3:bare") end
   end
 
   test "valid torrent file is decoded" do
     raw = File.read!("test/_data/ubuntu-14.04.4-desktop-amd64.iso.torrent")
-    assert Bento.torrent!(raw).__struct__ = Metainfo.Torrent
+    assert Bento.torrent!(raw).__struct__ == Metainfo.Torrent
   end
 
 end
