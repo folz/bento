50b0fab18f8e98d8d93925167c4be46cbfdd480b
Rodney Folz <rodney@rodneyfolz.com>
2016-05-21 11:46:30 -0700

    Properly alias the Torrent struct

--- test/bento/metainfo_test.exs
+++ test/bento/metainfo_test.exs
@@ -1,8 +1,8 @@
 defmodule Bento.MetainfoTest do
   use ExUnit.Case, async: true
 
-  import Bento.Metainfo
   alias Bento.MetainfoError
+  alias Bento.Metainfo.Torrent
 
   test "invalid torrent file raises an error" do
     assert_raise MetainfoError, fn -> Bento.torrent!("d3:foo3:bare") end
@@ -10,7 +10,7 @@ defmodule Bento.MetainfoTest do
 
   test "valid torrent file is decoded" do
     raw = File.read!("test/_data/ubuntu-14.04.4-desktop-amd64.iso.torrent")
-    assert Bento.torrent!(raw).__struct__ == Metainfo.Torrent
+    assert Bento.torrent!(raw).__struct__ == Torrent
   end
 
 end
